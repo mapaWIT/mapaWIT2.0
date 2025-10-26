4f33e9b03e0b70bb9c9c836a9d89f64767ec5e9b
Johsh926 <joshuaporras12345@gmail.com>
2025-10-27 00:06:09 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 //const contentSecurityPolicy = require('helmet-csp');
 app.use(cors());
 const router = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
 app.use('/api', router);
 
